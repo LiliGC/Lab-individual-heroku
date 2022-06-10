46680920bbd974bdb46fbf3f6f144c0b46c2b624
LiliGC <liliana.garmendia.c@gmail.com>
2022-06-10 01:53:55 -0400

    elimine archivos innecesarios

--- proyecto1/urls.py
+++ proyecto1/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('labtienda.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 
 if settings.DEBUG:
